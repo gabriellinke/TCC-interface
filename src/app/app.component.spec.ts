b43fff192b6ef240b05fa78cbeadcb3d4fac189b
gabriellinke <gh.linke@hotmail.com>
2024-09-29 18:37:22 -0300

    Update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'camera-test' title`, () => {
+  it(`should have the 'tcc-interface' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('camera-test');
+    expect(app.title).toEqual('tcc-interface');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, camera-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tcc-interface');
   });
 });
