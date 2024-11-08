787d21048fa91c0238bbb285150bbfe8ccf8aad2
gabriellinke <gh.linke@hotmail.com>
2024-11-08 19:00:57 -0300

    Add new build

--- dist/tcc-interface/server/main.server.mjs
+++ dist/tcc-interface/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RA57SXBT.mjs";import"./chunk-LDFQIA4I.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-OZK3FVPY.mjs";import"./chunk-LDFQIA4I.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
