bc34ad5c6c7958dd3a1e381f54c6938661b6af52
gabriellinke <gh.linke@hotmail.com>
2024-11-10 16:38:21 -0300

    Add content to info pages

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './faq.component.html',
   styleUrl: './faq.component.css'
 })
--- src/app/unserviceable-assets-info/unserviceable-assets-info.component.ts
+++ src/app/unserviceable-assets-info/unserviceable-assets-info.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-unserviceable-assets-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './unserviceable-assets-info.component.html',
   styleUrl: './unserviceable-assets-info.component.css'
 })
