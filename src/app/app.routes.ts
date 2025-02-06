d6a0d3538258822ff6eaa4b2628f1ed4b9e2c684
gabriellinke <gh.linke@hotmail.com>
2025-02-05 21:12:19 -0300

    Delete old authentication pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InfoComponent } from './info/info.component';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
 import { FileGenerationComponent } from './file-generation/file-generation.component';
 import { FileInfoComponent } from './file-info/file-info.component';
 import { AssetInfoComponent } from './asset-info/asset-info.component';
 import { SearchComponent } from './search/search.component';
-import { authGuard } from './auth.guard';
 import { UnserviceableAssetsInfoComponent } from './unserviceable-assets-info/unserviceable-assets-info.component';
 import { FAQComponent } from './faq/faq.component';
 import { ReportsRequestInfoComponent } from './reports-request-info/reports-request-info.component';
@@ -16,14 +13,6 @@ export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'sign-up',
-    component: SignUpComponent,
   },
   {
     path: 'generate',
