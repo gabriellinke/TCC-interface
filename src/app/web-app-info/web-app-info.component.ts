d9f1eeb0d0e9437404b5a9f17fb34a3faa7e9d09
gabriellinke <gh.linke@hotmail.com>
2025-02-08 18:03:50 -0300

    Update web app info page

--- src/app/web-app-info/web-app-info.component.ts
+++ src/app/web-app-info/web-app-info.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-web-app-info',
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './web-app-info.component.html',
   styleUrl: './web-app-info.component.css'
 })
