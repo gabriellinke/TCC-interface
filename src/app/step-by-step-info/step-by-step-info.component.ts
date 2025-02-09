ddf412d55fde734687b4a31395dd33b9b954705f
gabriellinke <gh.linke@hotmail.com>
2025-02-09 01:44:01 -0300

    Update step by step info page

--- src/app/step-by-step-info/step-by-step-info.component.ts
+++ src/app/step-by-step-info/step-by-step-info.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-step-by-step-info',
-  imports: [],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterLink],
+
   templateUrl: './step-by-step-info.component.html',
   styleUrl: './step-by-step-info.component.css'
 })
